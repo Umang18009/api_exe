c1a7ac8bc6148d74a8aba15d6b9c0879259773f9
umang18009 <umang.pipaliya@reformiqo.com>
2024-06-11 10:54:24 +0530

    As on 11-06-2024 10:54:00

--- lib/map_exe.dart
+++ lib/map_exe.dart
@@ -29,6 +29,16 @@ class _MapExeState extends State<MapExe> {
   List<TestMap> tempList = [TestMap(name: "name", mobile: "mobile", age: 1)];
 
 
+  JsonData dd = JsonData(userid: 1, id: 2, title: "title", body: "body");
+
+  JsonData ddd = JsonData.fromJson({
+    "userId": 1,
+    "id": 1,
+    "title": "pradip aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  });
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -39,13 +49,15 @@ class _MapExeState extends State<MapExe> {
             padding: const EdgeInsets.all(8.0),
             child: Column(
               children: [
-                Text("${m1['key_1']}",style: TextStyle(fontSize: 22),),
-                Text("${testMap.mobile}",style: TextStyle(fontSize: 22),),
-                Text("${tempList[0].name}",style: TextStyle(fontSize: 22),),
-                Text("userId : ${apiTemp['userId']}",style: TextStyle(fontSize: 22),),
-                Text("title : ${apiTemp['title']}",style: TextStyle(fontSize: 22),),
-                SizedBox(height: 10,),
-                Text("body : ${apiTemp['body']}",style: TextStyle(fontSize: 22),),
+                // Text("${m1['key_1']}",style: TextStyle(fontSize: 22),),
+                // Text("${testMap.mobile}",style: TextStyle(fontSize: 22),),
+                // Text("${tempList[0].name}",style: TextStyle(fontSize: 22),),
+                // Text("userId : ${apiTemp['userId']}",style: TextStyle(fontSize: 22),),
+                // Text("title : ${apiTemp['title']}",style: TextStyle(fontSize: 22),),
+                // SizedBox(height: 10,),
+                // Text("body : ${apiTemp['body']}",style: TextStyle(fontSize: 22),),
+                SizedBox(height: 30,),
+                Text("===[${ddd.id}]===")
               ],
             ),
           ),
@@ -62,4 +74,26 @@ class TestMap {
 
   TestMap({required this.name, required this.mobile, required this.age});
 
+}
+
+
+class JsonData {
+  int? userid;
+  int? id;
+  String? title;
+  String? body;
+
+  JsonData({
+    required this.userid,
+    required this.id,
+    required this.title,
+    required this.body,
+});
+
+  JsonData.fromJson(Map<String,dynamic> json){
+    userid = json['userId'];
+    id = json['id'];
+    title = json['title'];
+    body = json['body'];
+  }
 }
